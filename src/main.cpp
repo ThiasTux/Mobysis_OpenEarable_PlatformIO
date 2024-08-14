26393b08f532854f7fadc3dc9d39df583581b586
Mathias Ciliberto <mathias.ciliberto@gmail.com>
2024-08-14 13:53:15 +0100

    Added debugging option to main file

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,26 @@
 #include <SPI.h>
 #include "OpenEarable.h"
 
+// Set DEBUG to true in order to enable debug print
+#define DEBUG true
+
+// Change name to OELeft or OERight before flashing ("OpenEarable" if left as default value)
+String d_name = "OpenEarable";
+
 void setup()
 {
-    open_earable.begin();
+#if DEBUG
+  Serial.begin(115200);
+  delay(5000);
+  open_earable.debug(Serial);
+  delay(5000);
+#endif
+
+    open_earable.begin(d_name);
 }
 
 void loop()
 {
+  // Update and then sleep
     open_earable.update();
 }
